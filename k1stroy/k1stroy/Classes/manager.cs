5ac6d590c78f21629a108936521aa67d45a96a06
XtEnD0 <mika1achykel@gmail.com>
2025-06-13 07:41:50 +0300

    aware

--- k1stroy/k1stroy/Classes/manager.cs
+++ k1stroy/k1stroy/Classes/manager.cs
@@ -13,20 +13,5 @@ namespace k1stroy.Classes
         public static int Leave;
         public static Data.Users CurrentUser { get; set; }
         public static Frame MainFrame { get; set; }
-
-        //public static void GetImageData()
-        //{
-        //    var list = Data.k1stroyDBEntities.GetContext().Products.ToList();
-        //    foreach (var item in list)
-        //    {
-        //        string path = Directory.GetCurrentDirectory() + @"\img\" + item.ProductPhotoName;
-        //        if (File.Exists(path))
-        //        {
-        //            item.Photo = File.ReadAllBytes(path);
-        //        }
-        //        Data.k1stroyDBEntities.GetContext().SaveChanges();
-        //    }
-        //}
-
     }
 }
